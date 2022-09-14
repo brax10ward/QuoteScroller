c2644e10728ef70aab1b0eee0b74f5c45d1f2b0a
Braxton Ward <bward@grio.com>
2022-09-13 20:48:22 -0800

    Initial Commit

---
+++ Quote Scroller/Quote_ScrollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quote_ScrollerApp.swift
+//  Quote Scroller
+//
+//  Created by Braxton Ward on 9/13/22.
+//
+
+import SwiftUI
+
+@main
+struct Quote_ScrollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
